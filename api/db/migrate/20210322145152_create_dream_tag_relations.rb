4f9b02beab5a108f3a8fc9cb8d6c04a24684e477
kazutoichikawa <74768712+kazutoichikawa@users.noreply.github.com>
2021-03-22 23:53:46 +0900

    マイグレート

--- api/db/migrate/20210322145152_create_dream_tag_relations.rb
+++ api/db/migrate/20210322145152_create_dream_tag_relations.rb
@@ -1,7 +1,8 @@
 class CreateDreamTagRelations < ActiveRecord::Migration[6.1]
   def change
     create_table :dream_tag_relations do |t|
-
+      t.references :dream, foreign_key: true
+      t.references :tag, foreign_key: true
       t.timestamps
     end
   end
